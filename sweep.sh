f3f741bd3dbc86bcbd7e343bf68c1c6a6e0963ef
Rayan Syed <rsyed@bu.edu>
2024-07-01 14:15:37 -0400

    Renamed Files

--- sweep.sh
+++ sweep.sh
@@ -11,11 +11,11 @@ module load python3/3.10.12
 source .venv/bin/activate
 
 # Run python script to init sweep and print id, capture id (last line - tail)
-id=$(python sweep.py | tail -n 1)
+id=$(python id.py | tail -n 1)
 
 # "Save ID and print to terminal
 export SWEEP_ID=$id
 echo "SWEEP_ID: $id"
 
 # Run qsub file that runs wandb agent, pass ID with -v
-qsub -N "${jobname}" -o "/projectnb/tianlabdl/rsyed/wandb_sweep/logs/${jobname}.qlog" -v SWEEP_ID "newsweep.qsub"
+qsub -N "${jobname}" -o "/projectnb/tianlabdl/rsyed/wandb_sweep/logs/${jobname}.qlog" -v SWEEP_ID "sweep.qsub"
